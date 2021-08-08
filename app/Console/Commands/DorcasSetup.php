cf629b236adfd931837300ba879dc89bab18bc0d
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-08-08 11:06:06 +0100

    firstTime db creation clause; welcome email trigger

--- app/Console/Commands/DorcasSetup.php
+++ app/Console/Commands/DorcasSetup.php
@@ -56,231 +56,261 @@ class DorcasSetup extends Command
         // default setup
         $database_old = $this->option('database');
         $database = getenv('DB_DATABASE');
-
-
         $databaseHub = getenv('DB_HUB_DATABASE');
 
-        $this->info('Checking / Creating HUB Database');
-        if (!$databaseHub) {
-            $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
-            return;
-        }
 
-        try {
-            $conn = mysqli_connect(getenv('DB_HUB_HOST'), getenv('DB_HUB_USERNAME'), getenv('DB_HUB_PASSWORD'));
-
-            if (!$conn) {
-                die("Connection to HUB failed: " . mysqli_connect_error());
-            }
-            
-            // Create database
-            $sql = "CREATE DATABASE IF NOT EXISTS `" . $databaseHub . "`";
-            if (mysqli_query($conn, $sql)) {
-                $this->info(sprintf('Successfully created %s database', $databaseHub));
-            } else {
-                $this->error(sprintf('Error creating %s database, %s', $databaseHub, mysqli_error($conn)));
-            }
-            
-            mysqli_close($conn);
-
-        } catch (Exception $exception) {
-            $this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
-        }
+        $firstTimeCore = $this->checkDB($database);
+        $firstTimeHub = $this->checkDB($databaseHub);
 
 
-        $this->info('Importing HUB database...');
-        try {
-            $filename = resource_path('hub.sql');
-            # get the filename
-            if (!file_exists($filename)) {
-                throw new FileNotFoundException('Could not find the hub.sql database file at: '.$filename);
-            }
-            if (!is_readable($filename)) {
-                throw new FileException('The core.sql ('.$filename.') file is not readable by the process.');
+        if ($firstTimeHub) {
+
+            $this->info('Checking / Creating HUB Database');
+            if (!$databaseHub) {
+                $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
+                return;
             }
 
+            try {
+                $conn = mysqli_connect(getenv('DB_HUB_HOST'), getenv('DB_HUB_USERNAME'), getenv('DB_HUB_PASSWORD'));
 
-            $connImport = mysqli_connect(getenv('DB_HUB_HOST'), getenv('DB_HUB_USERNAME'), getenv('DB_HUB_PASSWORD'));
-
-            if (!$connImport) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-
-            $sql = "USE `" . $databaseHub . "`";
-            if (mysqli_query($connImport, $sql)) {
-                $this->info(sprintf('Successfully selected %s database', $databaseHub));
-            } else {
-                $this->error(sprintf('Error selecting %s database, %s', $databaseHub, mysqli_error($connImport)));
-            }
-
-            $queryLines = 0;
-            $tempLine = '';
-            // Read in the full file
-            $lines = file($filename);
-            // Loop through each line
-            foreach ($lines as $line) {
-
-                // Skip it if it's a comment
-                if (substr($line, 0, 2) == '--' || $line == '')
-                    continue;
-
-                // Add this line to the current segment
-                $tempLine .= $line;
-                // If its semicolon at the end, so that is the end of one query
-                if (substr(trim($line), -1, 1) == ';')  {
-                    // Perform the query
-                    mysqli_query($connImport, $tempLine) or $this->error(sprintf("Error in " . $tempLine .": %s", mysqli_error($connImport)));
-                    
-                    // Reset temp variable to empty
-                    $tempLine = '';
-                    $queryLines++;
+                if (!$conn) {
+                    die("Connection to HUB failed: " . mysqli_connect_error());
                 }
-            }
-
-            $this->info(sprintf('%s SQL lines imported successfully to HUB', $queryLines));
-
-            mysqli_close($connImport);
-
-        } catch (Exception $exception) {
-            $this->error(sprintf('Failed to import HUB database: %s', $exception->getMessage()));
-        }
-
-
-
-        $this->info('Checking / Creating CORE Database');
-        if (!$database) {
-            $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
-            return;
-        }
-
-        try {
-            $conn = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
-
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-            
-            // Create database
-            $sql = "CREATE DATABASE IF NOT EXISTS `" . $database . "`";
-            if (mysqli_query($conn, $sql)) {
-                $this->info(sprintf('Successfully created %s database', $database));
-            } else {
-                $this->error(sprintf('Error creating %s database, %s', $database, mysqli_error($conn)));
-            }
-            
-            mysqli_close($conn);
-
-        } catch (Exception $exception) {
-            $this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
-        }
-
-        $this->info('Importing CORE database...');
-        try {
-            $filename = resource_path('core.sql');
-            # get the filename
-            if (!file_exists($filename)) {
-                throw new FileNotFoundException('Could not find the core.sql database file at: '.$filename);
-            }
-            if (!is_readable($filename)) {
-                throw new FileException('The core.sql ('.$filename.') file is not readable by the process.');
-            }
-
-
-            $connImport = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
-
-            if (!$connImport) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-
-            $sql = "USE `" . $database . "`";
-            if (mysqli_query($connImport, $sql)) {
-                $this->info(sprintf('Successfully selected %s database', $database));
-            } else {
-                $this->error(sprintf('Error selecting %s database, %s', $database, mysqli_error($connImport)));
-            }
-
-            $queryLines = 0;
-            $tempLine = '';
-            // Read in the full file
-            $lines = file($filename);
-            // Loop through each line
-            foreach ($lines as $line) {
-
-                // Skip it if it's a comment
-                if (substr($line, 0, 2) == '--' || $line == '')
-                    continue;
-
-                // Add this line to the current segment
-                $tempLine .= $line;
-                // If its semicolon at the end, so that is the end of one query
-                if (substr(trim($line), -1, 1) == ';')  {
-                    // Perform the query
-                    mysqli_query($connImport, $tempLine) or $this->error(sprintf("Error in " . $tempLine .": %s", mysqli_error($connImport)));
-                    
-                    // Reset temp variable to empty
-                    $tempLine = '';
-                    $queryLines++;
+                
+                // Create database
+                $sql = "CREATE DATABASE IF NOT EXISTS `" . $databaseHub . "`";
+                if (mysqli_query($conn, $sql)) {
+                    $this->info(sprintf('Successfully created %s database', $databaseHub));
+                } else {
+                    $this->error(sprintf('Error creating %s database, %s', $databaseHub, mysqli_error($conn)));
                 }
+                
+                mysqli_close($conn);
+
+            } catch (Exception $exception) {
+                $this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
             }
 
-            $this->info(sprintf('%s SQL lines imported successfully', $queryLines));
 
-            mysqli_close($connImport);
+            $this->info('Importing HUB database...');
+            try {
 
-        } catch (Exception $exception) {
-            $this->error(sprintf('Failed to import database: %s', $exception->getMessage()));
+                $filename = resource_path('hub.sql');
+                # get the filename
+                if (!file_exists($filename)) {
+                    throw new FileNotFoundException('Could not find the hub.sql database file at: '.$filename);
+                }
+                if (!is_readable($filename)) {
+                    throw new FileException('The core.sql ('.$filename.') file is not readable by the process.');
+                }
+
+
+                $connImport = mysqli_connect(getenv('DB_HUB_HOST'), getenv('DB_HUB_USERNAME'), getenv('DB_HUB_PASSWORD'));
+
+                if (!$connImport) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "USE `" . $databaseHub . "`";
+                if (mysqli_query($connImport, $sql)) {
+                    $this->info(sprintf('Successfully selected %s database', $databaseHub));
+                } else {
+                    $this->error(sprintf('Error selecting %s database, %s', $databaseHub, mysqli_error($connImport)));
+                }
+
+                $queryLines = 0;
+                $tempLine = '';
+                // Read in the full file
+                $lines = file($filename);
+                // Loop through each line
+                foreach ($lines as $line) {
+
+                    // Skip it if it's a comment
+                    if (substr($line, 0, 2) == '--' || $line == '')
+                        continue;
+
+                    // Add this line to the current segment
+                    $tempLine .= $line;
+                    // If its semicolon at the end, so that is the end of one query
+                    if (substr(trim($line), -1, 1) == ';')  {
+                        // Perform the query
+                        mysqli_query($connImport, $tempLine) or $this->error(sprintf("Error in " . $tempLine .": %s", mysqli_error($connImport)));
+                        
+                        // Reset temp variable to empty
+                        $tempLine = '';
+                        $queryLines++;
+                    }
+                }
+
+                $this->info(sprintf('%s SQL lines imported successfully to HUB', $queryLines));
+
+                mysqli_close($connImport);
+
+            } catch (Exception $exception) {
+                $this->error(sprintf('Failed to import HUB database: %s', $exception->getMessage()));
+            }
+
         }
 
-        $this->info('Setting up OAuth and Administrative Account...');
-        try {
-            $init = new AuthInit();
-            $setup = json_decode($init->setup()); // this one doesnt seem to return what we want o
+        if ($firstTimeCore) {
+        
+            $this->info('Checking / Creating CORE Database');
+            if (!$database) {
+                $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
+                return;
+            }
 
-            //create for mobile - use default
+            try {
+                $conn = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
 
-            //manually get client ids & secret in first password grant client record
-            $client = DB::table("oauth_clients")->where('password_client', 1)->first();
-            $client_id = $client->id;
-            $client_secret = $client->secret;
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+                
+                // Create database
+                $sql = "CREATE DATABASE IF NOT EXISTS `" . $database . "`";
+                if (mysqli_query($conn, $sql)) {
+                    $this->info(sprintf('Successfully created %s database', $database));
+                } else {
+                    $this->error(sprintf('Error creating %s database, %s', $database, mysqli_error($conn)));
+                }
+                
+                mysqli_close($conn);
 
-            //$this->info(' ID: ' . $client_id . ", Secret: " . $client_secret);
+            } catch (Exception $exception) {
+                $this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
+            }
 
-            $password = \Illuminate\Support\Str::random(10);
+            $this->info('Importing CORE database...');
+            try {
 
-            $data = [
-                "firstname" => "Admin",
-                "lastname" => "User",
-                "email" => "demo@dorcas.io",
-                "installer" => "true",
-                "domain" => getenv('DORCAS_BASE_DOMAIN'),
-                "password" => $password,
-                "company" => "Demo",
-                "phone" => "08012345678",
-                "feature_select" => "all",
-                "client_id" => $client_id,
-                "client_secret" => $client_secret,
-            ];
-
-            $register = new AuthRegister();
-            $request = new \Illuminate\Http\Request($data);
-            $fractal = new \League\Fractal\Manager;
-            $user = $register->register($request, $fractal);
+                $filename = resource_path('core.sql');
+                # get the filename
+                if (!file_exists($filename)) {
+                    throw new FileNotFoundException('Could not find the core.sql database file at: '.$filename);
+                }
+                if (!is_readable($filename)) {
+                    throw new FileException('The core.sql ('.$filename.') file is not readable by the process.');
+                }
 
 
-        } catch (Exception $exception) {
-            $this->error(sprintf('Failed setting up OAuth: %s', $exception->getMessage()));
-        }
+                $connImport = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
 
-        $this->info('Creating Lumen App Key...');
-        $key = \Illuminate\Support\Str::random(32);
-        $path = base_path('.env');
-        if (file_exists($path)) {
-            file_put_contents($path, str_replace(
-                'APP_KEY=', 'APP_KEY='.$key, file_get_contents($path)
-            ));
-            $this->info('Successfully created APP KEY');
+                if (!$connImport) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "USE `" . $database . "`";
+                if (mysqli_query($connImport, $sql)) {
+                    $this->info(sprintf('Successfully selected %s database', $database));
+                } else {
+                    $this->error(sprintf('Error selecting %s database, %s', $database, mysqli_error($connImport)));
+                }
+
+                $queryLines = 0;
+                $tempLine = '';
+                // Read in the full file
+                $lines = file($filename);
+                // Loop through each line
+                foreach ($lines as $line) {
+
+                    // Skip it if it's a comment
+                    if (substr($line, 0, 2) == '--' || $line == '')
+                        continue;
+
+                    // Add this line to the current segment
+                    $tempLine .= $line;
+                    // If its semicolon at the end, so that is the end of one query
+                    if (substr(trim($line), -1, 1) == ';')  {
+                        // Perform the query
+                        mysqli_query($connImport, $tempLine) or $this->error(sprintf("Error in " . $tempLine .": %s", mysqli_error($connImport)));
+                        
+                        // Reset temp variable to empty
+                        $tempLine = '';
+                        $queryLines++;
+                    }
+                }
+
+                $this->info(sprintf('%s SQL lines imported successfully', $queryLines));
+
+                mysqli_close($connImport);
+
+            } catch (Exception $exception) {
+                $this->error(sprintf('Failed to import database: %s', $exception->getMessage()));
+            }
+
+            $this->info('Setting up OAuth and Administrative Account...');
+            try {
+
+                $init = new AuthInit();
+                $setup = json_decode($init->setup()); // this one doesnt seem to return what we want o
+
+                //create for mobile - use default
+
+                //manually get client ids & secret in first password grant client record
+                $client = DB::table("oauth_clients")->where('password_client', 1)->first();
+                $client_id = $client->id;
+                $client_secret = $client->secret;
+
+                //$this->info(' ID: ' . $client_id . ", Secret: " . $client_secret);
+
+                $password = \Illuminate\Support\Str::random(10);
+
+                $data = [
+                    "firstname" => "Admin",
+                    "lastname" => "User",
+                    "email" => "demo@dorcas.io",
+                    "installer" => "true",
+                    "domain" => getenv('DORCAS_BASE_DOMAIN'),
+                    "password" => $password,
+                    "company" => "Demo",
+                    "phone" => "08012345678",
+                    "feature_select" => "all",
+                    "client_id" => $client_id,
+                    "client_secret" => $client_secret,
+                ];
+
+                if (!empty(getenv('ADMINISTRATOR_EMAIL'))) {
+                    $data["trigger_event"] = 1;
+                }
+
+                $register = new AuthRegister();
+                $request = new \Illuminate\Http\Request($data);
+                $fractal = new \League\Fractal\Manager;
+                $user = $register->register($request, $fractal);
+
+
+
+            } catch (Exception $exception) {
+                $this->error(sprintf('Failed setting up OAuth: %s', $exception->getMessage()));
+            }
+
+            $this->info('Creating Lumen App Key...');
+            $key = \Illuminate\Support\Str::random(32);
+            $path = base_path('.env');
+            if (file_exists($path)) {
+                file_put_contents($path, str_replace(
+                    'APP_KEY=', 'APP_KEY='.$key, file_get_contents($path)
+                ));
+                $this->info('Successfully created APP KEY');
+            }
+
         }
 
     }
+
+
+    public function checkDB($dbName)
+    {
+         $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME =  ?";
+         $db = DB::select($query, [$dbName]);
+         if (empty($db)) {
+             return true;
+         } else {
+             return false;
+         }
+    }
+
 
 }
